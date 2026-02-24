26bc75838479e3d9daa5a2e210db72b0fcd3ad53
sudzekai <yomakum@mail.ru>
2026-02-24 08:47:08 +0300

    настроен gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import plugin from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [plugin()],
     server: {
+        base: '/L8rWeb',
         host: 'webshvets.ru',
         port: 80,
-        allowedHosts: ['webshvets.ru'],
+        allowedHosts: ['webshvets.ru', "sudzekai.github.io"],
         strictPort: true   // не менять порт, если занят
     }
 })
